c474940dabaa286482f4169e9ccbeba04e921189
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2023-12-21 21:41:27 +0100

    Day 20

---
+++ Day19/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Data;
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+var separator = Array.FindIndex(input, string.IsNullOrEmpty);
+
+var rules = input[..separator].Select(Rule.Parse).ToDictionary(r => r.Name);
+var parts = input[(separator + 1)..].Select(Part.Parse).ToList();
+
+int total = 0;
+
+foreach (var part in parts)
+{
+    var rule = "in";
+
+    while (rule != "A" && rule != "R")
+    {
+        rule = rules[rule].Apply(part);
+    }
+
+    if (rule == "A")
+    {
+        total += part.Values.Values.Sum();
+    }
+}
+
+var answer1 = total;
+Console.WriteLine($"Answer 1: {answer1}");
+
+class Step
+{
+    public required string Left { get; set; }
+    public int Right { get; set; }
+    public char Operator { get; set; }
+
+    public required string Target { get; set; }
+
+    public static Step Parse(string x)
+    {
+        var match = Regex.Match(x, @"((?<left>[xmas])(?<op>[<>])(?<right>\d+):)?(?<target>\w+)");
+
+        var left = match.Groups["left"].Value;
+        var op = match.Groups["op"].Value;
+        var right = match.Groups["right"].Value;
+        var target = match.Groups["target"].Value;
+
+        return new()
+        {
+            Left = left,
+            Right = right.Length > 0 ? int.Parse(right) : 0,
+            Operator = op.Length > 0 ? op[0] : '\0',
+            Target = target
+        };
+    }
+}
+
+class Rule
+{
+    public required string Name { get; set; }
+    public required List<Step> Steps { get; set; }
+
+    public string Apply(Part part)
+    {
+        foreach (var step in Steps)
+        {
+            var match = step.Operator switch
+            {
+                '<' => part.Values[step.Left] < step.Right,
+                '>' => part.Values[step.Left] > step.Right,
+                _ => true
+            };
+
+            if (match)
+            {
+                return step.Target;
+            }
+        }
+
+        throw new();
+    }
+
+    public static Rule Parse(string X)
+    {
+        var parts = X.Split('{', ',', '}');
+
+        return new()
+        {
+            Name = parts[0],
+            Steps = parts[1..].Select(Step.Parse).ToList()
+        };
+    }
+}
+
+class Part
+{
+    public required Dictionary<string, int> Values { get; set; }
+
+    public static Part Parse(string x)
+    {
+        var matches = Regex.Matches(x, @"(?<key>[xmas])=(?<value>\d+)");
+
+        var keys = matches.SelectMany(m => m.Groups["key"].Captures.Select(c => c.Value)).ToList();
+        var values = matches.SelectMany(m => m.Groups["value"].Captures.Select(c => c.Value)).ToList();
+
+        return new()
+        {
+            Values = keys.Zip(values.Select(int.Parse), (k, v) => (k, v)).ToDictionary(kv => kv.k, kv => kv.v)
+        };
+    }
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,200 @@
+﻿using System.Data;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+var modules = input.Select(Module.Parse).ToDictionary(m => m.Name);
+
+foreach (var (name, module) in modules)
+{
+    module.Initialize(modules);
+}
+
+var history = new Dictionary<bool, long>
+{
+    [false] = 0,
+    [true] = 0,
+};
+
+var buttonPresses = 1000;
+
+for (int i = 0; i < buttonPresses; i++)
+{
+    Simulate(modules, history, new());
+}
+
+var answer1 = history[false] * history[true];
+Console.WriteLine($"Answer 1: {answer1}");
+
+// rx is low when all these are low
+// these are low periodic, so rx will be low
+// if we take the least common multiple
+List<string> targets = ["rk", "cd", "zf", "qx"];
+var values = new Dictionary<string, int>();
+
+while (true)
+{
+    buttonPresses++;
+    var seen = Simulate(modules, history, targets);
+
+    foreach (var target in seen)
+    {
+        if (!values.ContainsKey(target))
+        {
+            values[target] = buttonPresses;
+        }
+    }
+
+    if (values.Count == targets.Count)
+    {
+        // we have found all targets
+        break;
+    }
+}
+
+var answer2 = values.Values.Aggregate((BigInteger)1, (acc, value) => LeastCommonMultiple(acc, value));
+Console.WriteLine($"Answer 2: {answer2}");
+
+BigInteger LeastCommonMultiple(BigInteger a, BigInteger b)
+{
+    return a / BigInteger.GreatestCommonDivisor(a, b) * b;
+}
+
+HashSet<string> Simulate(Dictionary<string, Module> modules, Dictionary<bool, long> history, List<string> targets)
+{
+    var pulses = new Queue<Pulse>();
+
+    pulses.Enqueue(new()
+    {
+        Source = "button",
+        Destination = "broadcaster",
+        Value = false
+    });
+
+    var seen = new HashSet<string>();
+
+    while (pulses.Count > 0)
+    {
+        var pulse = pulses.Dequeue();
+        history[pulse.Value]++;
+
+        if (targets.Contains(pulse.Destination) && !pulse.Value)
+        {
+            seen.Add(pulse.Destination);
+        }
+
+        if (!modules.ContainsKey(pulse.Destination))
+        {
+            continue;
+        }
+
+        var nextPulses = modules[pulse.Destination].Process(pulse);
+
+        foreach (var next in nextPulses)
+        {
+            pulses.Enqueue(next);
+        }
+    }
+
+    return seen;
+}
+
+class Pulse
+{
+    public required string Source { get; set; }
+    public required string Destination { get; set; }
+    public bool Value { get; set; }
+}
+
+abstract class Module
+{
+    public string Name { get; set; } = "";
+    public List<string> Destinations { get; set; } = [];
+
+    public virtual void Initialize(Dictionary<string, Module> modules)
+    {
+    }
+
+    public abstract List<Pulse> Process(Pulse pulse);
+
+    protected List<Pulse> Send(bool value)
+    {
+        return Destinations.Select(dest => new Pulse
+        {
+            Source = Name,
+            Destination = dest,
+            Value = value
+        }).ToList();
+    }
+
+    public static Module Parse(string line)
+    {
+        var match = Regex.Match(line, @"^(?<type>[%&])?(?<name>\w+) -> ((?<dest>\w+)(, )?)+$");
+
+        var type = match.Groups["type"].Value;
+        var name = match.Groups["name"].Value;
+        var destinations = match.Groups["dest"].Captures.Select(c => c.Value).ToList();
+
+        Module module = type switch
+        {
+            "%" => new FlipFlop(),
+            "&" => new Conjunction(),
+            _ => new Broadcast()
+        };
+
+        module.Name = name;
+        module.Destinations = destinations;
+
+        return module;
+    }
+}
+
+class Broadcast : Module
+{
+    public override List<Pulse> Process(Pulse pulse)
+    {
+        return Send(pulse.Value);
+    }
+}
+
+class FlipFlop : Module
+{
+    private bool state;
+
+    public override List<Pulse> Process(Pulse pulse)
+    {
+        if (pulse.Value)
+        {
+            return [];
+        }
+
+        state = !state;
+
+        return Send(state);
+    }
+}
+
+class Conjunction : Module
+{
+    private Dictionary<string, bool> inputs { get; set; } = [];
+
+    public override void Initialize(Dictionary<string, Module> modules)
+    {
+        foreach (var (name, module) in modules)
+        {
+            if (module.Destinations.Contains(Name))
+            {
+                inputs.Add(name, false);
+            }
+        }
+    }
+
+    public override List<Pulse> Process(Pulse pulse)
+    {
+        inputs[pulse.Source] = pulse.Value;
+
+        var result = !inputs.Values.All(v => v);
+
+        return Send(result);
+    }
+}
