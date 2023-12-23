c6bd170b40cf7fa970f4022020b71d48fbb32203
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2023-12-23 11:09:33 +0100

    Day 19

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Text.RegularExpressions;
+using Range = (int from, int to);
 
 var input = File.ReadAllLines("input.txt");
 var separator = Array.FindIndex(input, string.IsNullOrEmpty);
@@ -26,6 +27,54 @@ foreach (var part in parts)
 
 var answer1 = total;
 Console.WriteLine($"Answer 1: {answer1}");
+
+var accepted = new List<Dictionary<string, Range>>();
+
+var toVisit = new Queue<(string target, Dictionary<string, Range> ranges)>(
+[
+    (
+        "in",
+        new()
+        {
+            ["x"] = (1, 4000),
+            ["m"] = (1, 4000),
+            ["a"] = (1, 4000),
+            ["s"] = (1, 4000)
+        }
+    )
+]);
+
+while (toVisit.Count > 0)
+{
+    var (target, ranges) = toVisit.Dequeue();
+
+    if (target == "A")
+    {
+        accepted.Add(ranges);
+
+        continue;
+    }
+    
+    if (target == "R")
+    {
+        continue;
+    }
+
+    var next = rules[target].Apply(ranges);
+
+    foreach (var nextItem in next)
+    {
+        toVisit.Enqueue(nextItem);
+    }
+}
+
+var answer2 = accepted.Sum(Combinations);
+Console.WriteLine($"Answer 2: {answer2}");
+
+long Combinations(Dictionary<string, Range> ranges)
+{
+    return ranges.Values.Select(r => r.to - r.from + 1L).Aggregate((a, b) => a * b);
+}
 
 class Step
 {
@@ -79,9 +128,74 @@ class Rule
         throw new();
     }
 
+    public List<Dictionary<string, Range>> Split(Dictionary<string, Range> ranges, string key, int value)
+    {
+        var result = new List<Dictionary<string, Range>>
+        {
+            ranges.ToDictionary(kv => kv.Key, kv => kv.Value),
+            ranges.ToDictionary(kv => kv.Key, kv => kv.Value)
+        };
+
+        result[0][key] = (ranges[key].from, value - 1);
+        result[1][key] = (value, ranges[key].to);
+
+        return result;
+    }
+
+    public List<(string target, Dictionary<string, Range> ranges)> Apply(Dictionary<string, Range> ranges)
+    {
+        var result = new List<(string target, Dictionary<string, Range> ranges)>();
+
+        foreach (var step in Steps)
+        {
+            switch (step.Operator)
+            {
+                case '<':
+                    if (ranges[step.Left].to < step.Right)
+                    {
+                        result.Add((step.Target, ranges));
+
+                        return result;
+                    }
+                    
+                    if (ranges[step.Left].from < step.Right)
+                    {
+                        var newRanges = Split(ranges, step.Left, step.Right);
+
+                        result.Add((step.Target, newRanges[0]));
+                        ranges = newRanges[1];
+                    }
+
+                    break;
+                case '>':
+                    if (ranges[step.Left].from > step.Right)
+                    {
+                        result.Add((step.Target, ranges));
+
+                        return result;
+                    }
+                    
+                    if (ranges[step.Left].to > step.Right)
+                    {
+                        var newRanges = Split(ranges, step.Left, step.Right + 1);
+
+                        result.Add((step.Target, newRanges[1]));
+                        ranges = newRanges[0];
+                    }
+
+                    break;
+                default:
+                    result.Add((step.Target, ranges));
+                    break;
+            }
+        }
+
+        return result;
+    }
+
     public static Rule Parse(string X)
     {
-        var parts = X.Split('{', ',', '}');
+        var parts = X.Split(new[] { '{', ',', '}' }, StringSplitOptions.RemoveEmptyEntries);
 
         return new()
         {
